526ace8f64bb268d908f0197e63502a3f8964be4
5115-Source <chegstad5115@student.whatcom.edu>
2025-02-01 17:20:25 -0800

    Added comments to show the general structure of all programs.

---
+++ IteratorTesting/FibanacciNum.py
@@ -0,0 +1,14 @@
+"""Define an iterator class named Fibonacci Iterator for iterating Fibonacci numbers.
+ The constructor takes an argument that specifies the limit of the maximum Fibonacci number.
+ For example, new FibonacciIterator(23302) creates an iterator that iterates Fibonacci numbers less than or equal to 23302.
+ Write a test program called FibanacciNum.java that uses this iterator to display all Fibonacci numbers less than or equal to 100000."""
+ 
+#Create a FibonacciIterator(maxNumber) class
+
+#Have that class create an iterator that iterates Fibonacci numbers less than or equal to maxNumber
+
+#Make some testing code in main method that explains directions to user, takes a users input for the maxNumber, and then prints out the iterating numbers
+
+#Then ask user if they want to iterate through all Fibonacci number less than or equal to 100000
+
+#Ask user if they want to iterate through another number or exit the program
---
+++ StackTesting/PythonStackVersion/DisplayPrimeNumbers.py
@@ -0,0 +1,7 @@
+#Make sure to use pythons built in stack for this version of the program
+
+#Create a loop that only generates prime numbers
+
+#Store each prime number in a stack
+
+#Print prime numbers from stack in a clean and formatted way, also make sure they are printed in descending order
